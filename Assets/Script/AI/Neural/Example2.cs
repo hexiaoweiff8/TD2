437f24c815356dfa5676efa07e4757af0ee2267e
hexw <hexw@51rongzi.com>
2018-07-05 22:41:23 +0800

    修改demo为四维度行为

--- Assets/Script/AI/Neural/Example2.cs
+++ Assets/Script/AI/Neural/Example2.cs
@@ -24,6 +24,16 @@ namespace Assets.Script.AI.Neural
         public GameObject RightSensor;
 
         /// <summary>
+        /// 上侧探针
+        /// </summary>
+        public GameObject UpSensor;
+
+        /// <summary>
+        /// 下侧探针
+        /// </summary>
+        public GameObject DownSensor;
+
+        /// <summary>
         /// 训练次数
         /// </summary>
         public int TrainTimes = 10000;
@@ -43,12 +53,17 @@ namespace Assets.Script.AI.Neural
         /// <summary>
         /// 网络输入数据
         /// </summary>
-        private float[] inData = new float[2];
+        private float[] inDatas = new float[4];
 
         /// <summary>
         /// 网络输出数据
         /// </summary>
-        private float[] outData = new float[1];
+        private float[] outDatas = new float[2];
+
+        /// <summary>
+        /// 训练数据
+        /// </summary>
+        private float[][] trainDatas = new float[4][];
 
 
 
@@ -56,6 +71,12 @@ namespace Assets.Script.AI.Neural
         {
             startPos = transform.position;
             startRotate = transform.rotation;
+
+            // 初始化训练数据
+            trainDatas[0] = new float[2] { 1, 0.5f };
+            trainDatas[1] = new float[2] { 0, 0.5f };
+            trainDatas[2] = new float[2] { 0.5f, 1 };
+            trainDatas[3] = new float[2] { 0.5f, 0 };
         }
 
 
@@ -64,50 +85,61 @@ namespace Assets.Script.AI.Neural
             // 计算当前位置与墙的距离
 
             // 向左侧探针前方发射射线
-            inData[0] = GetDistance(LeftSensor);
+            inDatas[0] = GetDistance(LeftSensor);
 
             // 向右侧探针前方发射射线
-            inData[1] = GetDistance(RightSensor);
+            inDatas[1] = GetDistance(RightSensor);
+
+            // 向上侧探针前方发射射线
+            inDatas[2] = GetDistance(UpSensor);
+
+            // 向下侧探针前方发射射线
+            inDatas[3] = GetDistance(DownSensor);
 
 
-            if (inData[0] < 0.00001)
+            for (var i = 0; i < inDatas.Length; i++)
             {
-                // 如果碰到左侧墙壁
-                // 重置位置
-                transform.position = startPos;
-                transform.rotation = startRotate;
-                // 训练网络
-                for (var i = 0; i < TrainTimes; i++)
+                var inData = inDatas[i];
+                if (inData < 0.2)
                 {
-                    NeuralMono.Train(inData, new[] {1f});
-                }
-            }
-            else if (inData[1] < 0.00001)
-            {
-                // 如果碰到右侧墙壁
-                // 重置位置
-                transform.position = startPos;
-                transform.rotation = startRotate;
-                // 训练网络
-                for (var i = 0; i < TrainTimes; i++)
-                {
-                    NeuralMono.Train(inData, new[] { 0f });
+                    // 如果碰到左侧墙壁
+                    // 重置位置
+                    transform.position = startPos;
+                    transform.rotation = startRotate;
+                    // 训练网络
+                    for (var j = 0; j < TrainTimes; j++)
+                    {
+                        NeuralMono.Train(inDatas, trainDatas[i]);
+                    }
+                    print("←" + inDatas[0] + ",→" + inDatas[1] + ",上" + inDatas[2] + ",下" + inDatas[3]);
+                    Debug.LogError("----训练----" + i + "," + trainDatas[i][0] + "," + trainDatas[i][1]);
+                    break;
                 }
             }
 
 
             // 计算网络
-            outData = NeuralMono.Calculate(inData);
-            print(outData[0]);
+            outDatas = NeuralMono.Calculate(inDatas);
+            //print("" + outData[0] + "," + outData[1]);
 
             // 判断转向
-            if (outData[0] < 0.2f)
+            if (outDatas[0] < 0.2f)
             {
-                transform.Rotate(0, ((outData[0] / 1) * 0.2f) * -50 * Time.deltaTime, 0);
+                transform.Rotate(0, ((outDatas[0] / 1) * 0.2f) * -50 * Time.deltaTime, 0);
             }
-            else if(outData[0] > 0.7f)
+            else if(outDatas[0] > 0.7f)
             {
-                transform.Rotate(0, ((outData[0] / 1) * 0.2f + 0.8f) * 50 * Time.deltaTime, 0);
+                transform.Rotate(0, ((outDatas[0] / 1) * 0.2f + 0.8f) * 50 * Time.deltaTime, 0);
+            }
+
+
+            if (outDatas[1] < 0.2f)
+            {
+                transform.Rotate(((outDatas[0] / 1) * 0.2f) * -50 * Time.deltaTime, 0, 0);
+            }
+            else if (outDatas[1] > 0.7f)
+            {
+                transform.Rotate(((outDatas[0] / 1) * 0.2f + 0.8f) * 50 * Time.deltaTime, 0, 0);
             }
 
             // 位置前进
@@ -117,12 +149,79 @@ namespace Assets.Script.AI.Neural
         }
 
 
+        void OnGUI()
+        {
+            GUI.color = Color.red;
+            // TODO 绘制神经网络的结构
+            GUI.Label(new Rect(100, 100, 500, 800), GetNeuronNetStructureStr());
+        }
+
+
+        /// <summary>
+        /// 获取神经网络结构字符串
+        /// </summary>
+        /// <returns></returns>
+        private string GetNeuronNetStructureStr()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.Append("in:" + inDatas[0] + "," + inDatas[1] + "," + inDatas[2] + "," + inDatas[3]);
+            result.Append("-----------------------\n");
+            // 遍历input节点
+            foreach (var inItem in NeuralMono.NeuralNet.InLayer.NodeList)
+            {
+
+                foreach (var axon in inItem.FromList)
+                {
+                    result.Append(inItem.Value);
+                    result.Append("  :  ");
+                    result.Append(axon.weight);
+                    result.Append(",\n");
+                }
+            }
+            result.Append("-----------------------\n");
+
+            // 遍历hide节点
+            foreach (var hiddenLayer in NeuralMono.NeuralNet.HideLayer)
+            {
+                foreach (var hiddenItem in hiddenLayer.NodeList)
+                {
+                    foreach (var axon in hiddenItem.FromList)
+                    {
+                        result.Append(hiddenItem.Value);
+                        result.Append("  :  ");
+                        result.Append(axon.weight);
+                        result.Append(",\n");
+                    }
+                }
+            }
+            result.Append("-----------------------\n");
+
+            // 遍历out节点
+            foreach (var outItem in NeuralMono.NeuralNet.OutLayer.NodeList)
+            {
+                foreach (var axon in outItem.FromList)
+                {
+                    result.Append(outItem.Value);
+                    result.Append("  :  ");
+                    result.Append(axon.weight);
+                    result.Append(",\n");
+                }
+            }
+
+            result.Append("\n-----------------------");
+            result.Append("out:" + outDatas[0] + "," + outDatas[1]);
+            return result.ToString();
+        }
+
+
         private float GetDistance(GameObject obj)
         {
             var result = 0f;
 
             Ray ray = new Ray(obj.transform.position, obj.transform.forward);
             RaycastHit hit;
+            Debug.DrawRay(obj.transform.position, obj.transform.forward, Color.red);
             if (Physics.Raycast(ray, out hit, 1000f))
             {
                 result = hit.distance;
